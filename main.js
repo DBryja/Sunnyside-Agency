5abc6f647428241a086f2fee2bd76dc927f4cbec
Dawid Bryja <dawbryja@gmail.com>
2022-09-26 20:55:51 +0200

    Completed html/css/js

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector(".burger").addEventListener("click", function () {
+  document.querySelector(".rolldown-menu").classList.toggle("active");
+  document.querySelector("header").classList.toggle("active");
+});
